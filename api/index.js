ba2f9d06dad2d1ac33865bab82c7481749ceb881
PARMINDER SOKHAL <parmindersokhal22@gmail.com>
2025-01-24 23:15:32 +0530

    first

---
+++ api/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import UserRouter from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("server is running on 3000");
+});
+
+app.use("/api/user", UserRouter);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
